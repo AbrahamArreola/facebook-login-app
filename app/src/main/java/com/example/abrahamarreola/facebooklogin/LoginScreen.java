37239a868fdba191d72b243ea84196e70647a631
Abraham Arreola <dinowar_abraham@hotmail.com>
2019-07-04 00:39:14 -0500

    fixing some input text appareances and behaviors

--- app/src/main/java/com/example/abrahamarreola/facebooklogin/LoginScreen.java
+++ app/src/main/java/com/example/abrahamarreola/facebooklogin/LoginScreen.java
@@ -1,13 +1,124 @@
 package com.example.abrahamarreola.facebooklogin;
 
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginScreen extends AppCompatActivity {
+
+    TextInputLayout emailLayout, passLayout;
+    TextInputEditText emailInput, passInput;
+    Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+        emailLayout = (TextInputLayout) findViewById(R.id.email_layout);
+        emailInput = (TextInputEditText)findViewById(R.id.email_input);
+        passLayout = (TextInputLayout) findViewById(R.id.password_layout);
+        passInput = (TextInputEditText)findViewById(R.id.password_input);
+        submitButton = (Button) findViewById(R.id.login_button);
+
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailLayout.getEditText().getText().toString().trim();
+                String password = passLayout.getEditText().getText().toString().trim();
+                Boolean loginProcedure = true;
+
+                if(email.isEmpty()){
+                    emailLayout.setError("Username required");
+                    loginProcedure = false;
+                }
+
+                if(password.isEmpty()){
+                    passLayout.setError("Password required");
+                    loginProcedure = false;
+                }
+
+                if(loginProcedure) login(email, password);
+            }
+        });
+
+        emailInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                emailLayout.setErrorEnabled(false);
+            }
+        });
+
+        passInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void afterTextChanged(Editable editable) { passLayout.setErrorEnabled(false); }
+        });
+    }
+
+    private void login(final String email, final String password){
+        final String url = "http://192.168.100.2:8000/login_facebook_app";
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(getBaseContext(), response, Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getBaseContext(), error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("email", email);
+                params.put("password", password);
+
+                return params;
+            }
+        };
+
+        requestQueue.add(postRequest);
     }
 }
